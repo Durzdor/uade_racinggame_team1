263432a5cc2b7ce5af2d417123f07e949dcb0fd1
Durzdor <Durzdor@users.noreply.github.com>
2020-11-08 18:25:37 -0300

    Tabla de posiciones
    No se ve en pantalla pero esta la logica para verse en el editor

---
+++ Assets/_Main/Scripts/Karts/CarController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public int currentWaypoint;
+    public int currentLap;
+    public Transform lastWaypoint;
+    public int nbWaypoint; //Set the amount of Waypoints
+
+    private static int WAYPOINT_VALUE = 100;
+    private static int LAP_VALUE = 10000;
+    private int cpt_waypoint = 0;
+
+    private void Start()
+    {
+        Initialize();
+    }
+
+    // Use this for initialization
+    public void Initialize()
+    {
+        currentWaypoint = 0;
+        currentLap = 0;
+        cpt_waypoint = 0;
+        lastWaypoint = GameManager.Instance.lastCheckpointInLap;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ItemBox")) return;
+        string otherTag = other.gameObject.tag;
+        currentWaypoint = System.Convert.ToInt32(otherTag);
+        if (currentWaypoint == 1 && cpt_waypoint == nbWaypoint)
+        {
+            // completed a lap, so increase currentLap;
+            currentLap++;
+            cpt_waypoint = 0;
+        }
+
+        if (cpt_waypoint == currentWaypoint - 1)
+        {
+            lastWaypoint = other.transform;
+            cpt_waypoint++;
+        }
+    }
+
+    public float GetDistance()
+    {
+        return (transform.position - lastWaypoint.position).magnitude + currentWaypoint * WAYPOINT_VALUE +
+               currentLap * LAP_VALUE;
+    }
+
+    public int GetCarPosition(CarController[] allCars)
+    {
+        float distance = GetDistance();
+        int position = 1;
+        foreach (CarController car in allCars)
+        {
+            if (car.GetDistance() > distance)
+                position++;
+        }
+
+        return position;
+    }
+}
---
+++ Assets/_Main/Scripts/Utilities/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [HideInInspector] public bool dontDestroyOnLoad;
+    public static GameManager Instance;
+    
+    public Transform lastCheckpointInLap;
+    public CarController[] allCars;
+    public CarController[] carOrder;
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+
+            if (dontDestroyOnLoad)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        LoadStats();
+    }
+
+    private void LoadStats()
+    {
+        Debug.Log("Gamemanager test");
+        
+    }
+
+    private void Start()
+    {
+        //Sets up the car objects
+        carOrder = new CarController[allCars.Length];
+        InvokeRepeating(nameof(ManualUpdate), 0.5f, 0.5f);
+    }
+    
+    private void ManualUpdate()
+    {
+        foreach (CarController car in allCars)
+        {
+            carOrder[car.GetCarPosition(allCars) - 1] = car;
+        }
+    }
+
+    private void Update()
+    {
+        Debug.Log($"{carOrder[0]} esta primero");
+    }
+}
