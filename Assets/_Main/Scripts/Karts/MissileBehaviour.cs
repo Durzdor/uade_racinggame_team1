5e79d9e618c6ceb6102576504c89e754d6bb32fd
Tomi <63859937+tomicancheff@users.noreply.github.com>
2020-11-11 23:23:15 -0300

    Script Misil
    Hice el Script pero no puedo asignarle el target
    -Acomode las boxes
    -Puse als fuentes en toda la UI
    -Ajuste la velocidad maxima del player

--- Assets/_Main/Scripts/Karts/MissileBehaviour.cs
+++ Assets/_Main/Scripts/Karts/MissileBehaviour.cs
@@ -4,5 +4,38 @@ using UnityEngine;
 
 public class MissileBehaviour : MonoBehaviour
 {
-    //poner waypoints y esas cosas aca
+    public Transform RocketTarget;
+    public Rigidbody RocketRgb;
+
+    public float turnSpeed = 1f;
+    public float rocketFlySpeed = 10f;
+
+    private Transform rocketLocalTrans;
+
+    private void Start()
+    {
+        if (!RocketTarget)
+        {
+            Debug.Log("Bow1");
+
+            rocketLocalTrans = GetComponent<Transform>();
+
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (!RocketRgb)
+        
+            return;
+                RocketRgb.velocity = rocketLocalTrans.forward * rocketFlySpeed;
+
+        var rocketTargetRot = Quaternion.LookRotation(RocketTarget.position - rocketLocalTrans.position);
+        RocketRgb.MoveRotation(Quaternion.RotateTowards(rocketLocalTrans.rotation, rocketTargetRot, turnSpeed));
+        
+    }
+
+
+
 }
