edfe2c4e052b536bcaf6d48319e3c32ade4fcb5e
Durzdor <Durzdor@users.noreply.github.com>
2020-11-11 22:14:55 -0300

    SoundFix
    deje el sonido como estaba antes de romperlo todo :)

--- Assets/_Main/Scripts/Karts/MissilePower.cs
+++ Assets/_Main/Scripts/Karts/MissilePower.cs
@@ -9,21 +9,11 @@ public class MissilePower : Power
     [SerializeField] private GameObject missilePrefab;
     [SerializeField] private float missileDuration = 10f;
 
-    public override void Awake()
-    {
-        base.Awake();
-        audioSource = missilePrefab.GetComponent<AudioSource>();
-    }
 
     public override void Execute()
     {
         //Spawns shield at the specified location
         cloneMissilePrefab = Instantiate(missilePrefab,parent.transform.position,Quaternion.Euler(90,0,0));
-        // Sound effect
-        if (name == "MainPlayer")
-        {
-            audioSource.Play();
-        }
         //Destroys shield after the duration
         Object.Destroy(cloneMissilePrefab, missileDuration);
         //Turns off the object
--- Assets/_Main/Scripts/Karts/ShieldPower.cs
+++ Assets/_Main/Scripts/Karts/ShieldPower.cs
@@ -9,21 +9,10 @@ public class ShieldPower : Power
     [SerializeField] private GameObject shieldPrefab;
     [SerializeField] private float shieldDuration = 2f;
 
-    public override void Awake()
-    {
-        base.Awake();
-        audioSource = shieldPrefab.GetComponent<AudioSource>();
-    }
-
     public override void Execute()
     {
         // Spawns shield at the specified location
         cloneShieldPrefab = Instantiate(shieldPrefab,parent.transform.position,Quaternion.Euler(-90,0,0 ),parent.transform);
-        // Sound effect
-        if (name == "MainPlayer")
-        {
-            audioSource.Play();
-        }
         // Destroys shield after the duration
         Object.Destroy(cloneShieldPrefab, shieldDuration);
         // Turns off the object
